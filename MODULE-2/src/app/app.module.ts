bd887732bd0bba39dd07d8515cd08243adafa9ae
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-06-04 11:57:01 +0530

    Module updated

---
+++ MODULE-2/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningComponent } from './server/warning-alert/warning.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    WarningComponent,
+    CockpitComponent,
+    ServerElementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- MODULE1/src/app/app.module.ts
+++ MODULE1/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningComponent } from './server/warning-alert/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    WarningComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
